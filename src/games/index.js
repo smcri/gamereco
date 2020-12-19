51b08aed43ad218301a6bb8ce4b41852c2b3e30d
Sankalp <sankalprajendran@gmail.com>
2020-12-19 17:34:04 +0530

    games added

---
+++ src/games/index.js
@@ -0,0 +1,147 @@
+const gamesDB = [ 
+    { 
+        game: "Mancala",  
+        category: ["African","Egyptian"], 
+        gameId: "1",
+        genre: ["strategy"],
+        difficulty: "3",
+        playSite: "https://mancala.playdrift.com",
+        wiki: "en.wikipedia.org/wiki/Mancala"
+    }, 
+    { 
+        game: "Seega",  
+        category: ["Egyptian"], 
+        gameId: "2",
+        genre: ["strategy"],
+        difficulty: "3",
+        playSite: "",
+        wiki: "https://boardgamegeek.com/boardgame/11347/seega"
+    }, 
+    { 
+        game: "Hounds & Jackals",  
+        category: ["Egyptian"], 
+        gameId: "3",
+        genre: ["strategy","racing"],
+        difficulty: "3",
+        playSite: "http://www.playonlinedicegames.com/hounds-and-jackals",
+        wiki: "https://en.wikipedia.org/wiki/Hounds_and_Jackals"
+    }, 
+    { 
+        game: "Senet",  
+        category: ["Egyptian"], 
+        gameId: "4",
+        genre: ["strategy","racing"],
+        difficulty: "3",
+        playSite: "http://www.playonlinedicegames.com/senet",
+        wiki: "https://en.wikipedia.org/wiki/Senet"
+    }, 
+    { 
+        game: "Royal game of Ur",  
+        category: ["Sumerian","Middle Eastern","Mesopotamia"], 
+        gameId: "5",
+        genre: ["strategy"],
+        difficulty: "3",
+        playSite: "https://royalur.net/",
+        wiki: "https://en.wikipedia.org/wiki/Royal_Game_of_Ur"
+    }, 
+    { 
+        game: "Nard",  
+        category: ["Persian"], 
+        gameId: "6",
+        genre: ["strategy","Abstract"],
+        difficulty: "3",
+        playSite: "https://nardgammon.com/en/",
+        wiki: "https://en.wikipedia.org/wiki/Nard_(game)"
+    }, 
+    { 
+        game: "Rota",  
+        category: ["Roman"], 
+        gameId: "7",
+        genre: ["strategy"],
+        difficulty: "3",
+        playSite: "https://www.tinkercad.com/things/eglM7dBqmvj-rota-board-game",
+        wiki: ""
+    }, 
+    { 
+        game: "Anti-Monopoly",  
+        category: ["Modern"], 
+        gameId: "8",
+        genre: ["strategy","trading"],
+        difficulty: "3",
+        playSite: "https://www.iplay.com/games/anti-opoly-the-anti-monopoly-game",
+        wiki: "https://en.wikipedia.org/wiki/Anti-Monopoly"
+    }, 
+    { 
+        game: "Game of Life",  
+        category: ["Modern"], 
+        gameId: "9",
+        genre: ["luck","economic"],
+        difficulty: "3",
+        playSite: "https://playgameoflife.com/",
+        wiki: "https://en.wikipedia.org/wiki/The_Game_of_Life"
+    }, 
+    { 
+        game: "Cluedo",  
+        category: ["Modern"], 
+        gameId: "10",
+        genre: ["strategy","mystery"],
+        difficulty: "3",
+        playSite: "https://www.flashgames.jp/Cluedo-online",
+        wiki: "https://en.wikipedia.org/wiki/Cluedo"
+    }, 
+    
+    { 
+        game: "Yahtzee",  
+        category: ["Modern"], 
+        gameId: "11",
+        genre: ["luck","family"],
+        difficulty: "3",
+        playSite: "https://cardgames.io/yahtzee/",
+        wiki: "https://en.wikipedia.org/wiki/Yahtzee"
+    }, 
+    { 
+        game: "Carcassonne",  
+        category: ["Modern"], 
+        gameId: "12",
+        genre: ["strategy","city building"],
+        difficulty: "3",
+        playSite: "https://www.kongregate.com/games/badim/carcassonne",
+        wiki: "https://en.wikipedia.org/wiki/Carcassonne_(board_game)"
+    }, 
+    { 
+        game: "Dead of Winter",  
+        category: ["Modern"], 
+        gameId: "13",
+        genre: ["strategy","cooperative"],
+        difficulty: "3",
+        playSite: "https://www.fantasyflightgames.com/en/products/dead-of-winter/",
+        wiki: ""
+    }, 
+    { 
+        game: "Twilight Struggle",  
+        category: ["Modern"], 
+        gameId: "14",
+        genre: ["strategy","political","wargame"],
+        difficulty: "3",
+        playSite: "https://store.steampowered.com/app/406290/Twilight_Struggle/",
+        wiki: "https://en.wikipedia.org/wiki/Twilight_Struggle"
+    }, 
+    { 
+        game: "Euphoria: Build a Better Dystopia",  
+        category: ["Modern"], 
+        gameId: "15",
+        genre: ["strategy","science fiction"],
+        difficulty: "3",
+        playSite: "https://store.steampowered.com/app/417300/Tabletop_Simulator__Euphoria_Build_a_Better_Dystopia/",
+        wiki: ""
+    }, 
+    { 
+        game: "Antiquity",  
+        category: ["Modern"], 
+        gameId: "16",
+        genre: ["strategy","civilization","city building"],
+        difficulty: "3",
+        playSite: "http://play.boardgamecore.net/",
+        wiki: ""
+    }, 
+];
